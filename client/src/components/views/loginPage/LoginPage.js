a0d73c1b8708cafe131b87ffb8ddef32c0be4164
ablued <skwodhkswkd@naver.com>
2021-04-15 15:41:18 +0900

    로그인 기능 구현

--- client/src/components/views/loginPage/LoginPage.js
+++ client/src/components/views/loginPage/LoginPage.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import { loginUser } from '../../../_actions/user_action'
+function LoginPage(props) {
+    const dispatch = useDispatch();
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
 
-function LoginPage(params) {
+    const onEmailHandler = (e) => {
+        setEmail(e.currentTarget.value)
+    }
+    const onPasswordHandler = (e) => {
+        setPassword(e.currentTarget.value)
+    }
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+        dispatch(loginUser(body))
+        .then(response => {
+            if(response.payload.loginSuccess){
+                props.history.push('/')     // react에서는 페이지 이동을 이런식으로 한다.
+            } else {
+                alert('Error')
+            }
+        });
+
+    }
     return(
-        <div>
-            LoginPage
+        <div style={{
+            display:'flex', justifyContent: 'center', alignItems:'center',
+            width:'100%', height:'100vh'
+        }}>
+            <form style={{display:'flex', flexDirection:'column'}} onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type="Email" value={Email} onChange={onEmailHandler}/>
+                <label>Password</label>
+                <input type="Password" value={Password} onChange={onPasswordHandler}/>
+                <br/>
+                <button>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -11,12 +11,12 @@ import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers';
 
-const createStoreWithMiddleware = applyMiddleware()
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 ReactDOM.render(
   <Provider
     store = {createStoreWithMiddleware(Reducer,
-      window.__REDUX_DEVTOOLS-EXTENSION__&&
-      window.__REDUX_DEVTOOLS-EXTENSION__()
+      window.__REDUX_DEVTOOLS_EXTENSION__&&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
       )}
   >
     <App />
